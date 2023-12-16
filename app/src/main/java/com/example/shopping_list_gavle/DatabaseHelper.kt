693e40498f6a3a6e5bd0ebfc587e6d16bf742cd2
Toros Kutlu <toros_kutlu@hotmail.com>
2023-12-16 23:57:15 +0100

    Initial commit App Android Studio Hedgehog | 2023.1.1 - API 26 ("Oreo", Andoid 8.0). Empty Activity. Kotlin. SQLite.

---
+++ app/src/main/java/com/example/shopping_list_gavle/DatabaseHelper.kt
@@ -0,0 +1,50 @@
+package com.example.shopping_list_gavle
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        private const val DATABASE_NAME = "shoppingList.db"
+        private const val DATABASE_VERSION = 1
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        // SQL-skript för att skapa tabeller
+        db.execSQL("""
+            CREATE TABLE items (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                category TEXT,
+                datetime_added DATETIME DEFAULT CURRENT_TIMESTAMP
+            );
+        """)
+        db.execSQL("""
+            CREATE TABLE deleted (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                item_id INTEGER NOT NULL,
+                name TEXT NOT NULL,
+                category TEXT,
+                datetime_deleted DATETIME DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (item_id) REFERENCES items (id)
+            );
+        """)
+        db.execSQL("""
+            CREATE TABLE purchased (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                item_id INTEGER NOT NULL,
+                name TEXT NOT NULL,
+                category TEXT,
+                datetime_purchased DATETIME DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (item_id) REFERENCES items (id)
+            );
+        """)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        // Här kan du hantera uppdateringar av databasens schema
+    }
+}
